b37e2977fdbbbef19f13f1884ffb07639eb2c9c6
William Richardson <wwd9135@gmail.com>
2026-03-31 09:37:13 +0100

    Update Iboss_DLLCheck.ps1

--- PowerShell_Projects/EndpointCompliance/Intune-ProactiveRemediationScripts/Detections/Iboss_DLLCheck.ps1
+++ PowerShell_Projects/EndpointCompliance/Intune-ProactiveRemediationScripts/Detections/Iboss_DLLCheck.ps1
@@ -1,13 +1,18 @@
-#### 2. DLL Version Check
-$IbsaDLL = "$env:ProgramFiles\Phantom\IBSA\ibsa.dll"
+# Force 64-bit Program Files even when script runs in 32-bit Intune context
+$ProgramFiles64 = ${env:ProgramW6432}
+
+$IbsaDLL = "$ProgramFiles64\Phantom\IBSA\ibsa.dll"
 $MinVersion = [version]'6.5.253.0'
 
 if (Test-Path $IbsaDLL) {
-$CurrentVersion = [version](Get-Item $IbsaDLL).VersionInfo.FileVersionRaw
+    $CurrentVersion = [version](Get-Item $IbsaDLL).VersionInfo.FileVersionRaw
 }
+
 Write-Host $CurrentVersion
 Write-Host $MinVersion
+
 if ($CurrentVersion -lt $MinVersion) {
-Write-Host 1 # Reports non-compliance for outdated version
+    Write-Host 1  # NON-compliant
 } else {
-Write-Host 0 } # COMPLIANT: Tells Intune everything is fine
+    Write-Host 0  # Compliant
+}
