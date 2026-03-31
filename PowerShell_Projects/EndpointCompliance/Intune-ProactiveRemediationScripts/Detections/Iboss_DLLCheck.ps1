482de94b238103f505f1320bc4201699bd82ade3
William Richardson <wwd9135@gmail.com>
2026-03-31 09:13:11 +0100

    Update Iboss_DLLCheck.ps1

--- PowerShell_Projects/EndpointCompliance/Intune-ProactiveRemediationScripts/Detections/Iboss_DLLCheck.ps1
+++ PowerShell_Projects/EndpointCompliance/Intune-ProactiveRemediationScripts/Detections/Iboss_DLLCheck.ps1
@@ -1,11 +1,13 @@
 #### 2. DLL Version Check
 $IbsaDLL = "$env:ProgramFiles\Phantom\IBSA\ibsa.dll"
-$MinVersion = [version]'6.5.254.0'
+$MinVersion = [version]'6.5.253.0'
 
 if (Test-Path $IbsaDLL) {
 $CurrentVersion = [version](Get-Item $IbsaDLL).VersionInfo.FileVersionRaw
 }
+Write-Host $CurrentVersion
+Write-Host $MinVersion
 if ($CurrentVersion -lt $MinVersion) {
-exit 1 # Reports non-compliance for outdated version
+Write-Host 1 # Reports non-compliance for outdated version
 } else {
-exit 0 } # COMPLIANT: Tells Intune everything is fine
+Write-Host 0 } # COMPLIANT: Tells Intune everything is fine
