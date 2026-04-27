661b25c89b1926c0c124e7e40b979100d48d6abd
William Richardson <wwd9135@gmail.com>
2026-04-27 11:37:32 +0100

    Update Chrome_Arp.ps1

--- PowerShell_Projects/EndpointCompliance/Intune-ProactiveRemediationScripts/Detections/Chrome_Arp.ps1
+++ PowerShell_Projects/EndpointCompliance/Intune-ProactiveRemediationScripts/Detections/Chrome_Arp.ps1
@@ -1,12 +1,16 @@
-$Apps = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall) | Get-ItemProperty | Select-Object DisplayName
-$Apps += (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall) | Get-ItemProperty | Select-Object DisplayName
-if ((Test-Path "$env:ProgramFiles\Google\Chrome\Application\chrome.exe") -or (Test-Path "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe")) {
-    if ($null -eq ($apps | Where-Object DisplayName -EQ "Google Chrome")) {
-        Write-Output $false
-        }
-    Write-Output $true
-    } else {
-    Write-Output $true
-}
+$Apps  = (Get-ChildItem HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall) | 
+             Get-ItemProperty | Select-Object DisplayName
+$Apps += (Get-ChildItem HKLM:\SOFTWARE\WOW6432Node\Microsoft\Windows\CurrentVersion\Uninstall) | 
+             Get-ItemProperty | Select-Object DisplayName
 
-#taken from https://patchtuesday.com/blog/tech-blog/windows-defender-exploit-guard-breaks-google-chrome/
+$pathExists = (Test-Path "$env:ProgramFiles\Google\Chrome\Application\chrome.exe") -or 
+              (Test-Path "${env:ProgramFiles(x86)}\Google\Chrome\Application\chrome.exe")
+
+$inRegistry = $null -ne ($Apps | Where-Object DisplayName -EQ "Google Chrome")
+
+if ($pathExists -and $inRegistry) {
+    Write-Host "Google Chrome detected"
+    Exit 0
+} else {
+    Exit 1
+}
