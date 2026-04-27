38dd104424ddd8e4b76aa0c09dac3f8dfabed762
William Richardson <wwd9135@gmail.com>
2026-04-27 09:07:19 +0100

    Update Defender_MAPS_Connectivity.ps1

--- PowerShell_Projects/EndpointCompliance/Intune-ProactiveRemediationScripts/Detections/Defender_MAPS_Connectivity.ps1
+++ PowerShell_Projects/EndpointCompliance/Intune-ProactiveRemediationScripts/Detections/Defender_MAPS_Connectivity.ps1
@@ -1,16 +1,23 @@
 # 3. MAPS Connectivity
 $MpCmdRun = "$Env:ProgramFiles\Windows Defender\MpCmdRun.exe"
-
+$date = get-dateT
 if (-not (Test-Path $MpCmdRun)) {
     Write-Host "MpCmdRun.exe not found at $MpCmdRun" -ForegroundColor Red
-    exit 1
+    Write-Host 1
 } else {
     $Output   = & $MpCmdRun -ValidateMapsConnection 2>&1
     $ExitCode = $LASTEXITCODE
-
-    if ($ExitCode -eq 0 -or $Output -match "successfully established a connection") {
-        exit 0 # COMPLIANT: MAPS connectivity is working
-    } else {
-        exit 1 # NON-COMPLIANT: MAPS connectivity issue detected
+    $OutputStr = $Output -join " "  # Join array into single string
+    if ($ExitCode -eq 0 -or $OutputStr -match "successfully established a connection") {
+        Write-Host 0 # COMPLIANT: MAPS connectivity is working
+    } elseif ($OutputStr -match "ERROR: ValidateMapsConnection failed" -or $OutputStr -match "ERROR") {
+        if ($OutputStr -match '\[(\d{2}/\d{2}/\d{4}\s\d{2}:\d{2}:\d{2})\]') {
+            Write-Host "Timestamp: $($matches[1])"
+           
+            if ($matches[1] ) {}
+        } else {
+            Write-Host "No timestamp found in output"
+        }
+        Write-Host $OutputStr
     }
 }
