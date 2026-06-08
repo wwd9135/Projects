759eafb5919ee6f999abd4ba36dd7ca0838c5e80
William Richardson <wwd9135@gmail.com>
2026-06-08 12:19:13 +0100

    .,

--- PowerShell_Projects/DCUIntunePackaging/DCU_BIOSDetect.ps1
+++ PowerShell_Projects/DCUIntunePackaging/DCU_BIOSDetect.ps1
@@ -9,19 +9,19 @@ if (Get-ItemProperty -Path $RegPath -Name $RegName -ErrorAction SilentlyContinue
         $LastRunDate = [datetime]::Parse($LastRunDateString, [System.Globalization.CultureInfo]::GetCultureInfo('en-GB'))
     } catch {
         Write-Host "Invalid date format in registry."
-        exit 0
+        
     }
 
     $DaysSinceRun = (Get-Date) - $LastRunDate
 
     if ($DaysSinceRun.TotalDays -ge 14) {
         Write-Host "Two weeks have passed. Proceed to Remediation."
-        exit 1
+       
     } else {
         Write-Host "Less than two weeks. Skip Remediation."
-        exit 0
+       
     }
 } else {
     Write-Host "Registry key not found. Proceed."
-    exit 1
+
 }
